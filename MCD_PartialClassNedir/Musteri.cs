4be7f4a0930ee717d256bee06735434934e23e89
jackup <yk.58@hotmail.com>
2021-12-07 15:43:02 +0300

    Add project files.

---
+++ MCD_PartialClassNedir/Musteri.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PartialClassNedir
+{
+    class Musteri
+    {
+        #region Fields
+
+        public int id { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string emailAdres { get; set; }
+
+        #endregion
+
+        #region Metod
+        public int YeniKayit(Musteri m)
+        {
+            Console.WriteLine("Database kayıt işlemi tamamlandı.");
+            return 1;
+        }
+        public int KayitGuncelle(Musteri m)
+        {
+            Console.WriteLine("Müşteri kaydı güncellendi.");
+            return 1;
+        }
+        public int KayitSil(Musteri m)
+        {
+            Console.WriteLine("Müşteri kaydı silindi.");
+            return 1;
+        }
+        #endregion
+    }
+}
