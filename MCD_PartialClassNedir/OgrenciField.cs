4192bc81637c8888e302a3e81a6b3a8353e651c2
jackup <yk.58@hotmail.com>
2021-12-07 15:56:20 +0300

    code changed

---
+++ MCD_PartialClassNedir/OgrenciField.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PartialClassNedir
+{
+    public partial class Ogrenci
+    {
+        public int id { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string ogrenciNumarasi { get; set; }
+    }
+}
---
+++ MCD_PartialClassNedir/OgrenciMetod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PartialClassNedir
+{
+    public partial class Ogrenci
+    {
+        //Kontrol işlemleri: sistem içerisinde öğrenci kayıtlı mı? 71777000;
+        //Parametre kontrol işlemi: Giriş verileriniz hatalıdır. 71777001;
+        //Try catch: 71777002;
+        //Başarılı: 71777003
+        //
+        //71777004;
+
+        public int YeniKayit(Ogrenci o)
+        {
+            Console.WriteLine("Database kayıt işlemi tamamlandı.");
+            return 1;
+        }
+        public int KayitGuncelle(Ogrenci o)
+        {
+            Console.WriteLine("Öğrenci kaydı güncellendi.");
+            return 1;
+        }
+        public int KayitSil(Ogrenci o)
+        {
+            Console.WriteLine("Öğrenci kaydı silindi.");
+            return 1;
+        }
+    }
+}
